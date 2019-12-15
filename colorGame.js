8cb5144f235d72e64352c90f974f08c4600d6f74
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2019-12-15 22:26:46 +0530

    First Commit

---
+++ colorGame.js
@@ -0,0 +1,59 @@
+var colors = generateRandomColor(6);
+var squares = document.querySelectorAll(".square");
+var colorDisplay = document.querySelector("#colorDisplay");
+var correctColor = pickColor();
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#resetButton");
+colorDisplay.textContent = correctColor;
+
+resetButton.addEventListener("click", function() {
+  colors = generateRandomColor(6);
+  correctColor = pickColor();
+  colorDisplay.textContent = correctColor;
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = "#232323";
+});
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].style.backgroundColor = colors[i];
+  squares[i].addEventListener("click", function() {
+    var selectedColor = this.style.backgroundColor;
+    if (selectedColor === correctColor) {
+      messageDisplay.textContent = "Correct Choice";
+      changeColor();
+    } else {
+      messageDisplay.textContent = "Wrong Choice";
+      this.style.backgroundColor = "#232323";
+    }
+  });
+}
+
+function changeColor(color) {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = correctColor;
+    h1.style.backgroundColor = correctColor;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColor(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr[i] = rgbGenerator();
+  }
+  return arr;
+}
+
+function rgbGenerator() {
+  var red = Math.floor(Math.random() * 256 - 1);
+  var green = Math.floor(Math.random() * 256 - 1);
+  var blue = Math.floor(Math.random() * 256 - 1);
+  return "rgb(" + red + ", " + green + ", " + blue + ")";
+}
