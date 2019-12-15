fe086a768dae9f25c0b7262ba09447885d3dfc51
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2019-12-15 22:54:14 +0530

    Modified the font, changed the background color of the page

--- colorGame.js
+++ colorGame.js
@@ -14,7 +14,7 @@ resetButton.addEventListener("click", function() {
   for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
   }
-  h1.style.backgroundColor = "#232323";
+  h1.style.backgroundColor = "steelblue";
 });
 
 for (var i = 0; i < squares.length; i++) {
@@ -26,7 +26,7 @@ for (var i = 0; i < squares.length; i++) {
       changeColor();
     } else {
       messageDisplay.textContent = "Wrong Choice";
-      this.style.backgroundColor = "#232323";
+      this.style.backgroundColor = "white";
     }
   });
 }
